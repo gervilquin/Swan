603dcfe6cc0bf3aac3b99b37179e21da706b0a11
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-12-29 13:51:09 +0100

    Fixed matrix builder
    Class is not static anymore

--- FEM/Solver/MatrixBuilder.m
+++ FEM/Solver/MatrixBuilder.m
@@ -1,38 +1,63 @@
 classdef MatrixBuilder < handle
     % Class to build matrix for direct solution without reductions. 
 
-    methods (Static)
-        function genM = createGeneralMatrix(K, bc)
+    properties (Access = private)
+        K
+        bc
+        RHS
+        sK
+    end
+
+    methods (Access = public)
+        function obj = MatrixBuilder(s)
+            obj.init(s);
+        end
+
+        function genLHS = createLHS(obj)
+            Ct = obj.createConstraintMatrix;
+            genLHS = obj.createGeneralMatrix(Ct);
+        end
+
+        function genRHS = createRHS(obj)
+            genRHS = obj.createGeneralVector();
+        end
+    end
+
+    methods (Access = private)
+        function init(obj, cParams)
+            obj.K = cParams.K;
+            obj.bc = cParams.bc;
+            obj.RHS = cParams.RHS;
+            obj.sK = size(cParams.K, 1);
+        end
+
+        function generalM = createGeneralMatrix(obj, Ct)
             % Method to create the general matrix
-            sK = size(K, 1);
-            Ct = MatrixBuilder.buildCMatrix(bc, sK);
             C = Ct';
             sC1 = size(C, 1);
             sC2 = size(C, 2);
-            genM = zeros(sK+sC2);
-            genM(1:sK, 1:sK) = K;
-            genM(sK+1:end, 1:sC1) = C';
-            genM(1:sC1, sK+1:end) = C;
+            generalM = zeros(obj.sK+sC2);
+            generalM(1:obj.sK, 1:obj.sK) = obj.K;
+            generalM(obj.sK+1:end, 1:sC1) = C';
+            generalM(1:sC1, obj.sK+1:end) = C;
         end
 
-        function genFVector = createGeneralVector(RHS, bc)
-            dirichletValues = bc.dirichlet_values;
-            sRHS = size(RHS,1);
-            sDirichlet = size(dirichletValues, 1);
-            genFVector = zeros(sRHS+sDirichlet, 1);
-            genFVector(1:sRHS) = RHS;
-            genFVector(sRHS+1:end) = dirichletValues;
+        function fullRHS = createGeneralVector(obj)
+            constDisp = obj.bc.dirichlet_values;
+            sRHS = size(obj.RHS,1);
+            sDirichlet = size(constDisp, 1);
+            fullRHS = zeros(sRHS+sDirichlet, 1);
+            fullRHS(1:sRHS) = obj.RHS;
+            fullRHS(sRHS+1:end) = constDisp;
         end
 
-        function Ct = buildCMatrix(bc, sK)
-            % dirichlet: contains restricted dofs DOFr x 1
-            % dirichlet_values: contains displacement values DOFr x 1
-            dirichletDOFs = bc.dirichlet;
+        function Ct = createConstraintMatrix(obj)
+            dirichletDOFs = obj.bc.dirichlet;
             nDOFr = size(dirichletDOFs, 1);
-            Ct = zeros(nDOFr, sK);
+            Ct = zeros(nDOFr, obj.sK);
             for i = 1:nDOFr
-                Ct(i,dirichletDOFs(i)) = 1;
-            end        
+                 Ct(i,dirichletDOFs(i)) = 1; 
+            end     
         end
     end
 end
